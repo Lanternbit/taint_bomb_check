2ee9bbb646dad7f7d049b881e2e4a7d15fdd36ca
Lanternbit <seolg0810@gmail.com>
2024-10-22 00:04:23 +0900

    Update average_score.csv for unit

--- score.py
+++ score.py
@@ -13,21 +13,23 @@ for root, dirs, files in os.walk('results'):
             with open(file_path, 'r') as csvfile:
                 reader = csv.DictReader(csvfile)
                 scores = []
+
                 for row in reader:
                     # Score 열의 값을 float으로 변환하여 리스트에 추가
                     scores.append(float(row['Score']))
+                    unit = row['Unit']
 
                 # 평균 계산
                 if scores:
                     avg_score = mean(scores)
-                    results.append([file, avg_score])
+                    results.append([file, avg_score, unit])
 
 results.sort(key=lambda x: x[0])
 
 # 결과를 새 CSV 파일로 저장
 with open('average_scores.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
-    writer.writerow(['File Name', 'Average Score'])
+    writer.writerow(['File Name', 'Average Score', 'Unit'])
     writer.writerows(results)
 
 print("평균 점수가 'average_scores.csv' 파일로 저장되었습니다.")
